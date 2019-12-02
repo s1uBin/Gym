20f4afeda9c4419b7a2c77878c925c74e3663dd3
WeiBin <binwong112@gmail.com>
2019-12-03 01:47:03 +0800

    Initial Commit

---
+++ Gym/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gym
+//
+//  Created by user on 3/12/2019.
+//  Copyright © 2019 00557052. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
